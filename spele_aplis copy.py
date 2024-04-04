7c160fdc18eff7680803d3da3a59d23a2630eda8
stacija2 <>
2024-04-04 19:18:58 +0300

    jauna ievadisana ar bugu restartesana

--- spele_aplis copy.py
+++ spele_aplis copy.py
@@ -22,10 +22,13 @@ punkti = []
 gajiens=0
 selected_point = None
 state = None
-selected_point = None
 
-punktu_skaits: str = "Ievadiet lauciņu skaitu 15-25"
-SEARCH_DEPTH=4
+sobrid_ievada = 0
+ievadisanas_teksti = ["Ievadiet lauciņu skaitu (15-25)", "Kurš sāks pirmais? (1: Cilvēks, 2: AI)", "Lietot Alfa beta? (1: Jā, 2: Nē)"]
+ievaditas_veribas = {}
+ievadisanas_teksts = ""
+
+SEARCH_DEPTH=1
 def generate_points(punktu_sk):
     p = []
     for i in range(punktu_sk):
@@ -37,30 +40,47 @@ def generate_points(punktu_sk):
         )
     return p
 
-
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-        if state == None:
-            if event.type == pygame.KEYDOWN and event.unicode.isdigit():
-                if len(punktu_skaits) == 1:
-                    punktu_skaits += event.unicode
-                    punktu_sk = int(punktu_skaits)
-
-                    if punktu_sk < 6 or punktu_sk > 25:
-                        punktu_skaits = "Ievadiet lauciņu skaitu 15-25"
-                        continue
-
-                    punkti = generate_points(punktu_sk=punktu_sk)
-                    gameinfo=gametree.GameInfo(punktu_skaits=punktu_sk,sak_dators=False,izmantot_alfa_beta=True)
-                    state = gametree.GameState()
-                else:
-                    punktu_skaits = event.unicode
+        if state == None: 
             if event.type == pygame.KEYDOWN and event.key == pygame.K_BACKSPACE:
-                punktu_skaits = "Ievadiet lauciņu skaitu 15-25"
+                ievadisanas_teksts = ""
+
+            elif event.type == pygame.KEYDOWN and (event.key == pygame.K_KP_ENTER or event.key == pygame.K_RETURN):
+                
+                if sobrid_ievada == 0 and int(ievadisanas_teksts) >= 6 and int(ievadisanas_teksts) <= 25:
+                    ievaditas_veribas['punktu_sk'] = int(ievadisanas_teksts)
+                    sobrid_ievada+=1
+                elif sobrid_ievada == 1 and (int(ievadisanas_teksts) == 1 or int(ievadisanas_teksts) == 2):
+                    ievaditas_veribas['sak_dators'] = False if int(ievadisanas_teksts) == 1 else True 
+                    sobrid_ievada+=1
+                elif sobrid_ievada == 2 and (int(ievadisanas_teksts) == 1 or int(ievadisanas_teksts) == 2):
+                    ievaditas_veribas['izmantot_alfa-beta'] = True if int(ievadisanas_teksts) == 1 else False 
+                    sobrid_ievada+=1
+
+                ievadisanas_teksts = ""
+
+                if sobrid_ievada >= len(ievadisanas_teksti):
+                    punkti = generate_points(punktu_sk=ievaditas_veribas['punktu_sk'])
+                    gameinfo=gametree.GameInfo(punktu_skaits=ievaditas_veribas['punktu_sk'],sak_dators=ievaditas_veribas['sak_dators'],izmantot_alfa_beta=ievaditas_veribas['izmantot_alfa-beta'])
+                    state = gametree.GameState()
+                    sobrid_ievada = 0
+
+            elif event.type == pygame.KEYDOWN:
+                ievadisanas_teksts += event.unicode
+
         else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+                punkti = []
+                gajiens=0
+                selected_point = None
+                state = None
+                continue
+
+
             if( gajiens%2==1)==gameinfo.sak_dators:#speletaja gajiens
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if pygame.mouse.get_pressed()[0]:
@@ -96,7 +116,7 @@ while running:
 
     if state == None:
         font = pygame.font.SysFont(None, 48)
-        img = font.render(punktu_skaits, True, (255, 255, 255))
+        img = font.render(ievadisanas_teksti[sobrid_ievada] + ' - ' + ievadisanas_teksts, True, (255, 255, 50 * (sobrid_ievada + 2)))
         text_rect = img.get_rect(
             center=(screen.get_size()[0] / 2, screen.get_size()[1] / 2)
         )
@@ -129,6 +149,10 @@ while running:
         screen.blit(
             font.render("P2 punkti: " + str(state.p2_punkti), True, (255, 255, 255)),
             (25, 150),
+        )
+        screen.blit(
+            font.render("Restartēšanai: R", True, (255, 255, 255)),
+            (25, 200),
         )
 
         if len(state.linijas) == int(len(punkti) / 2):
